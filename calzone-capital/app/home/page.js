7a8fb8e3dd901be13023129539a5f3bb12ee2584
Tharidhu <tharidhuphigera@gmail.com>
2024-02-18 16:44:23 +0000

    frontEnd updated
    Added new feature of Real Time Prices using a websocket from TwelveData

--- calzone-capital/app/home/page.js
+++ calzone-capital/app/home/page.js
@@ -1,29 +1,67 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from "react";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Sidebar from '/components/Sidebar';
-import TrendingStocks from '/components/TrendingStocks';
 import News from '/components/News';
+import TwelveDataWebSocket from '../utils/websocket';
 
 const Home = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
+  const [priceData, setPriceData] = useState([]);
 
   if (!session) {
     router.push("/");
-    return null; 
+    return null;
   }
+
+  const handleWebSocketMessage = (data) => {
+    console.log('WebSocket Message in Home:', data);
+    if (data.event === 'price') {
+      setPriceData((prevData) => {
+        const updatedData = [...prevData];
+        const existingIndex = updatedData.findIndex((price) => price.symbol === data.symbol);
+  
+        if (existingIndex !== -1) {
+          updatedData[existingIndex] = data;
+        } else {
+          updatedData.push(data);
+        }
+  
+        return updatedData;
+      });
+    }
+  };
 
   return (
     <main className="flex min-h-screen bg-[#ffffff]">
       <Sidebar />
-      <section className='w-3/4 pl-12 pr-4 pt-4 overflow-y-auto bg-gray'>
+      <section className='w-3/4 pl-12 pr-4 overflow-y-auto bg-gray'>
+        <TwelveDataWebSocket symbols={['AAPL', 'RY', 'RY:TSX', 'EUR/USD', 'BTC/USD']} onMessage={handleWebSocketMessage} />
+        <div>
+          <h2 className='text-black text-2xl mb-2 pt-14'>Real-Time Prices</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 overflow-hidden bg-[#F5F7F8] rounded-md shadow-md">
+            {priceData.map((price, index) => (
+              <div key={index} className="news-card">
+                <div className="w-full h-full rounded-md relative p-6">
+                  <span className="absolute left-4 top-4 text-[#38bfc3] text-sm md:text-lg xl:text-xl 2xl:text-2xl">
+                    {price.symbol}
+                  </span>
+                  <div className="w-full h-full flex">
+                    <span className="mt-6 text-lg md:text-xl 2xl:text-2xl flex items-center">
+                      ${Math.abs(price.price).toFixed(2)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         <News />
-        {/* <TrendingStocks /> */}
       </section>
     </main>
   );
-};
+}  
 
 export default Home;
---
+++ calzone-capital/app/utils/websocket.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+const TwelveDataWebSocket = ({ symbols, onMessage }) => {
+  useEffect(() => {
+    const socket = new WebSocket(`wss://ws.twelvedata.com/v1/quotes/price?apikey=7b5861f61d974edeb58bd9da0cc76789`);
+
+    socket.addEventListener('open', (event) => {
+      console.log('WebSocket Connection Opened:', event);
+      socket.send(JSON.stringify({
+        action: 'subscribe',
+        params: { symbols: symbols.join(',') }
+      }));
+    });
+
+    socket.addEventListener('message', (event) => {
+      const data = JSON.parse(event.data);
+      console.log('WebSocket Message Received:', data);
+      onMessage(data);
+    });
+
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket Connection Closed:', event);
+    });
+
+    return () => {
+      socket.close();
+      console.log('WebSocket Connection Closed: Cleanup');
+    };
+  }, [symbols, onMessage]);
+
+  return null;
+};
+
+export default TwelveDataWebSocket;
